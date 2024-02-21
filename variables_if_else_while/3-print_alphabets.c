07a903692293044506b443460c285a7ffda089e1
Kevin Rentas <kevrentas3@gmailcom>
2024-02-21 10:53:01 -0400

    completed 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	char Alphabet = 'A';
+
+	while (alphabet <= 'z')
+		{
+		putchar(alphabet);
+		alphabet++;
+		}
+	while (Alphabet <= 'Z')
+	{
+		putchar(Alphabet);
+		Alphabet++;
+	}
+		putchar('\n');
+		return (0);
+}
