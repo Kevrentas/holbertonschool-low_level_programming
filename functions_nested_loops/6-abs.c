5eb1a5318c6e0f6fb5d54e1172bcdba9fc76ee40
Kevin Rentas <kevrentas3@gmailcom>
2024-02-23 17:22:51 -0400

    completed 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 
 /**
- * _abs
- * - check the code
- *
+ * _abs - check the code
+ * @k: an argument
  * Return: Always 0.
  */
-int _abs(int)
+int _abs(int k)
 {
+	if (k < 0)
+	{
+		k = (-1) * k;
+	}
+	return (0);
+}
