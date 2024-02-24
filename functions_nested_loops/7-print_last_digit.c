22afc86324800631de73207406f9de56305e9fe7
Kevin Rentas <kevrentas3@gmailcom>
2024-02-24 14:48:49 -0400

    completed 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @k: an argument
+ * Return: Always 0.
+ */
+int print_last_digit(int k)
+{
+	int f;
+
+	f= (k % 10);
+	if (f < 0)
+	{
+		f = (-1 * f);
+	}
+	_putchar('0' + f);
+	return (f);
+}
