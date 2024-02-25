f926fc8f798d298642e19cfe35bed08547577692
Kevin Rentas <kevrentas3@gmailcom>
2024-02-25 16:07:37 -0400

    completed 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_to_98 - check the code
+ * @n: an argument
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
