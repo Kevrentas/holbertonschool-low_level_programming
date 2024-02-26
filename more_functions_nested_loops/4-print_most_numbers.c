018e8c128c8eaa2e76cd9219b9e2174d37df34a4
Kevin Rentas <kevrentas3@gmailcom>
2024-02-26 13:36:20 -0400

    completed 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar((x) + '0');
+		}
+	}
+	_putchar('\n');
+}
