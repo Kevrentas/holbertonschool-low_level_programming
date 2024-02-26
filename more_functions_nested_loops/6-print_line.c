380618c8fa3b86896d73f5606f277288da493b1a
Kevin Rentas <kevrentas3@gmailcom>
2024-02-26 19:17:58 -0400

    completed 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,17 @@ void print_line(int n)
 {
 	int k;
 
-	for (k = 0; k < n; k++)
-	{
-		_putchar(95);
-	}
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (k = 0; k < n; k++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
 }
