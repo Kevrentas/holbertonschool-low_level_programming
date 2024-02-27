1200e326d7ccd658feda73db7a2d227d3a53a52d
Kevin Rentas <kevrentas3@gmailcom>
2024-02-27 14:05:41 -0400

    completed 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: argument
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
