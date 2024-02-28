8b17e4b63759ecaba56c9d4e17f14b36e3e31e49
Kevin Rentas <kevrentas3@gmailcom>
2024-02-28 10:15:14 -0400

    completed 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int count;
+	char num;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (count % 2 == 0)
+		{
+			num = str[count];
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
