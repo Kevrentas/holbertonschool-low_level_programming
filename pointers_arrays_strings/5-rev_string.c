7157b49342cef8d9b1fc49a905282a36a3d52d24
Kevin Rentas <kevrentas3@gmailcom>
2024-02-28 09:34:59 -0400

    completed 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,9 @@ void rev_string(char *s)
 	int f, r_s;
 	char character;
 
-	for (f = '\0'; s[f] != 0; f++)
+	for (f = 0; s[f] != '\0'; f++)
 
-		r_s = 0;
+	r_s = 0;
 	for (f = f - 1; r_s < f; r_s++)
 	{
 		character = s[f];
