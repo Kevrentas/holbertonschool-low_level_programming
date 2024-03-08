c9672dac2039ddc5a108c4a16a9ab9ce3ec873bb
Kevin Rentas <kevrentas3@gmailcom>
2024-03-08 15:16:57 -0400

    completed 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,19 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int i = 0;
+	char *array = NULL;
+	unsigned int i;
 
-	array = malloc(size * sizeof(c));
-
-	while (i < size)
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
 	{
-		if (array == NULL)
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
 		{
-			return (NULL);
+			for (i = 0; i < size; i++)
+				array[i] = c;
 		}
-		array[i] = c;
-		i++;
 	}
 	return (array);
 }
