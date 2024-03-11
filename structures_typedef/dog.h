46ec5dcb2fa936691dc0b9f1b5d96205326fa13e
Kevin Rentas <kevrentas3@gmailcom>
2024-03-11 11:34:00 -0400

    prep dogh

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - atributos del perro
+ * @name: como llamo el perro pa que venga
+ * @age: cuanto lleva vivo
+ * @owner: su maistro
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
