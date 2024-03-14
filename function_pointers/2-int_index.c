b6fa5d845ad5b4bd806b7dd2c162ed6ef5a215bc
Kevin Rentas <kevrentas3@gmailcom>
2024-03-14 14:00:45 -0400

    completed 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is equal to 98
+ * @array: duh
+ * @size: size of array
+ * @cmp: function ptr
+ * Return: index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1 || array == NULL || (*cmp) == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		(*cmp)(array[i]);
+		if ((*cmp)(array[i]) != 0)
+			break;
+	}
+	return (i);
+	if (i == '\0')
+		return (-1);
+}
